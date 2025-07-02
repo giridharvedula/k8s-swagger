53e38cdb63dda4af32746cbc302f67bc60f91f47
Giridhar Vedula <giridharvedula@gmail.com>
2025-07-02 20:53:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,53 @@
 package main
 
-import {
-  "crypto/tls"
-  "crypto/x509"
-  "fmt"
-  "io"
-  "log"
-  "net/http"
-  "os"
-}
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 const (
-  k8sAPI = "https://kubernetes.default.svc"
-  openapiPath = "/openapi/v2"
-  tokenPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-  caCertPath = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+	k8sAPI      = "https://kubernetes.default.svc"
+	openapiPath = "/openapi/v2"
+	tokenPath   = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	caCertPath  = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 )
 
 func openapiHandler(w http.ResponseWriter, r *http.Request) {
-  if r.Method == http.MethodOptions {
-    addCors(w)
-    w.WriteHeader(http.StatusNoContent)
-    return
-    }
+	if r.Method == http.MethodOptions {
+		addCORS(w)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	token, err := os.ReadFile(tokenPath)
+	if err != nil {
+		http.Error(w, "cannot read SA token", http.StatusInternalServerError)
+		return
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: getTLSConfig(caCertPath),
+		},
+	}
+
+	req, _ := http.NewRequest("GET", k8sAPI+openapiPath, nil)
+	req.Header.Set("Authorization", "Bearer "+string(token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "upstream error: "+err.Error(), http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+
+	addCORS(w)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, resp.Body)
+}
